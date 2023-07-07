aa0f694d82888194af1f09f85a27986b59df554c
Pikaa <furkansrds@gmail.com>
2023-07-07 17:18:11 +0300

    Add project files.

---
+++ OOP2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri() {Id = 1, MusteriNo = "123",
+                Adi = "Engin", 
+                Soyadi = "Demiroğ", 
+                TcNo = "12345678910"
+            };
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri() { Id = 2,
+                MusteriNo = "1234", 
+                SirketAdi = "Kodlama.io" ,
+                VergiNo = "1234567890"
+            };
+
+
+            // Gerçek müşteri - Tüzel müşteri
+            //SOLID
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(musteri1);
+            customerManager.Add(musteri2);
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileloggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            List<ILoggerService> loggers = new List<ILoggerService>() { smsLoggerService, databaseLoggerService };
+            basvuruManager.BasvuruYap(esnafKrediManager,loggers);
+
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+            Console.ReadLine();
+        }
+    }
+}
